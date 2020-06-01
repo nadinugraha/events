fdf032988e70bb0a48e2a87c5d06f1196b4d324e
narendra <nadinugraha@gmail.com>
2020-06-01 18:26:06 +0700

    commit

--- server.js
+++ server.js
@@ -5,31 +5,37 @@ const bodyParser = require('body-parser');
 const eventRegistration = require('./middlewares/eventRegistration');
 const orders = require('./data/orders');
 const emitter = require('./middlewares/emitter');
+
+const customers = require('./handlers/customers');
+const suppliers = require('./handlers/suppliers');
+const transporters = require('./handlers/transporters'); 
+
 app.use(bodyParser.json());
 //app.use(emitter);
 app.use(eventRegistration);
 
+app.post('/createorderwithoutevent', function(req,res){
+    const { trxorder } = req.body;
+    
+    //a new order is saved to any database/storage e.g : mySQL, postgres, mongodb, etc. 
+    orders.push(trxorder);
+    console.log(JSON.stringify(orders));
+
+    customers.notifyCustomer(trxorder.orderid);
+    suppliers.requestForStock(trxorder.orderid);
+    transporters.bookForDelivery(trxorder.orderid);
+
+})
 
 app.post('/createorderwithevent', function(req,res){
     const { trxorder } = req.body;
     
     //a new order is saved to any database/storage e.g : mySQL, postgres, mongodb, etc. 
     orders.push(trxorder);
-    
+    console.log(JSON.stringify(orders));
     
     emitter.emit('order_created', trxorder);
 })
-
-app.post('/createorderwithoutevent1', function(req,res){
-    const { trxorder } = req.body;
-    
-    //a new order is saved to any database/storage e.g : mySQL, postgres, mongodb, etc. 
-    orders.push(trxorder);
-    
-    
-    emitter.emit('order-created', trxorder);
-})
-
 
 app.use(function (req, res, next) {
         var err = new Error('Not Found');
